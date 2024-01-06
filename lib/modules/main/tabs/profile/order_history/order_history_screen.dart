39f5a16a3b2cdcc72caf0a6f90bda16f2d259a90
superxgh <mrsiwakorn@gmail.com>
2024-01-06 14:48:46 +0700

    update

--- lib/modules/main/tabs/profile/order_history/order_history_screen.dart
+++ lib/modules/main/tabs/profile/order_history/order_history_screen.dart
@@ -1,32 +1,41 @@
 import 'package:buaberry_mobile/config.dart';
-import 'order_history_controller.dart';
 
 class OrderHistoryScreen extends StatelessWidget {
-  final historyCtrl = Get.put(OrderHistoryController());
+  final orderHistoryController = Get.put(OrderHistoryController());
 
   OrderHistoryScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GetBuilder<OrderHistoryController>(builder: (_) {
-      return  Scaffold(
-          backgroundColor: appController.appTheme.foodPrimaryLightColor,
-          appBar: CommonAppBar(name: trans(FoodOrderingThemeFont.profile_menu_orders)),
-          body: historyCtrl.isShimmer ?const FoodHistoryShimmer() : ListView.builder(
-                  itemBuilder: (context, index) => HistoryCard(orderHistoryModel:historyCtrl.orderHistory[index])
-                          .paddingSymmetric(
-                              horizontal: Insets.i12, vertical: Insets.i15)
-                          // .decorated(
-                          //     color: appController.appTheme.whiteColor,
-                          //     borderRadius: BorderRadius.circular(AppRadius.r8))
-                          // .boxShadow(
-                          //     color: appController.appTheme.foodShadowColor,
-                          //     blurRadius: 6,
-                          //     offset: const Offset(2, 3))
-                          .paddingOnly(bottom: Insets.i20),
-                  itemCount: historyCtrl.orderHistory.length)
-              .marginSymmetric(horizontal: Insets.i15, vertical: Insets.i20),
-        );
+      return Scaffold(
+        backgroundColor: appController.appTheme.foodPrimaryLightColor,
+        appBar: CommonAppBar(
+            name: trans(FoodOrderingThemeFont.profile_menu_orders)),
+        body: orderHistoryController.isShimmer
+            ? const FoodHistoryShimmer()
+            : ListView.builder(
+                    itemBuilder: (context, index) => Container(
+                          decoration: BoxDecoration(
+                              color: appController.appTheme.whiteColor,
+                              borderRadius: BorderRadius.circular(AppRadius.r8),
+                              boxShadow: [
+                                BoxShadow(
+                                    color:
+                                        appController.appTheme.foodShadowColor,
+                                    blurRadius: 6,
+                                    offset: const Offset(2, 3))
+                              ]),
+                          child: HistoryCard(
+                                  orderHistoryModel:
+                                      orderHistoryController.orderHistory[index])
+                              .paddingSymmetric(
+                                  horizontal: Insets.i12, vertical: Insets.i15)
+                              .paddingOnly(bottom: Insets.i20),
+                        ),
+                    itemCount: orderHistoryController.orderHistory.length)
+                .marginSymmetric(horizontal: Insets.i15, vertical: Insets.i20),
+      );
     });
   }
 }
