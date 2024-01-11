a70dd67e6ccd64ad38cf1f735f9d1ba92e390af6
mr.reggie.duke <mr.reggie.duke@gmail.com>
2024-01-11 13:45:35 +0700

    Update home tab

--- lib/modules/main/tabs/home/home_controller.dart
+++ lib/modules/main/tabs/home/home_controller.dart
@@ -1,35 +1,32 @@
-
 import 'package:buaberry_mobile/config.dart';
 
 class HomeController extends GetxController {
-
   var featuredRestaurantList = <Product>[].obs;
   var mustTryList = <Product>[].obs;
   int current = 0;
   var foodBannerList = <FoodBannerModel>[].obs;
 
   final CarouselController carouselController = CarouselController();
-  var instructionList =[].obs;
+  var instructionList = [].obs;
   var nearByList = <Product>[].obs;
 
   @override
   void onReady() {
-
     getData();
-
     super.onReady();
   }
 
-  getData(){
+  getData() {
     instructionList.value = foAppArray.instructionList;
-  for (var i = 0; i < foAppArray.nearByRestaurant.length; i++) {
-    nearByList.add(Product.fromJson(foAppArray.nearByRestaurant[i]));
-  }
+    for (var i = 0; i < foAppArray.nearByRestaurant.length; i++) {
+      nearByList.add(Product.fromJson(foAppArray.nearByRestaurant[i]));
+    }
     for (var i = 0; i < foAppArray.bannerList.length; i++) {
       foodBannerList.add(FoodBannerModel.fromJson(foAppArray.bannerList[i]));
     }
     for (var i = 0; i < foAppArray.featuredRestaurant.length; i++) {
-      featuredRestaurantList.add(Product.fromJson(foAppArray.featuredRestaurant[i]));
+      featuredRestaurantList
+          .add(Product.fromJson(foAppArray.featuredRestaurant[i]));
     }
     for (var i = 0; i < foAppArray.mustTryList.length; i++) {
       mustTryList.add(Product.fromJson(foAppArray.featuredRestaurant[i]));
